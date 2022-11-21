b69b4abed146ca6765a5244ff83b8b9dd9285d21
Ginpin21 <iftekharahmed2003@gmail.com>
2022-11-21 20:25:02 +0400

    Fixed an error with mongo client

--- server.js
+++ server.js
@@ -8,12 +8,9 @@ app.use(express.json());
 app.use(require("./routes/record.js"));
 // get driver connection
 const dbo = require("./db/conn");
- 
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
+dbo.connectToServer(function (err) {
+  if (err) console.error(err);
+  app.listen(port,()=>{
+    console.log(`Server is running on port: ${port}`);
+  })
 });
